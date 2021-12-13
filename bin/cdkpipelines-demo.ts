ac61b1f1d34e7e6d5aa7ea5e1ff1f807faafc5db
Brent Fox <brentgfox@gmail.com>
2021-12-12 19:54:42 -0700

    Change to pipeline name.

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { App } from '@aws-cdk/core';
-import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
+import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-stack';
 
 const app = new App();
 
